1b018c2c0b58c6705661348d9eaafc032645c544
eman157 <eman.abdeltawab.maghraby@gmail.com>
2025-07-13 15:07:57 +0300

    Update fingerprint_app.py

--- fingerprint_app.py
+++ fingerprint_app.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # ----------------- Google Sheets Setup -----------------
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(r"D:\fingerprint_app.py\service_account.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
 client = gspread.authorize(creds)
 
 # Google Sheet
